5cca46cee06a630471888b51ce2ecddd39ba68a1
Taras2802 <taras1tkhir@gmail.com>
2023-06-06 14:32:24 +0300

    API

---
+++ MAIN.py
@@ -0,0 +1,10 @@
+import requests
+
+
+
+url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=USD&date=&json"
+url1 = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=PLN&date=&json"
+r = requests.get(url)
+r1 = requests.get(url1)
+print(r.json()[0]["rate"])
+print(r1.json()[0]["rate"])
